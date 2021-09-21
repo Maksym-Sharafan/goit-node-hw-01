c73657815f522398bb575e3f5439795f7adfc540
Maksym-Sharafan <magsimsharafa@gmail.com>
2021-09-21 10:35:17 +0300

    fixed

--- contacts.js
+++ contacts.js
@@ -1,5 +1,6 @@
 const fs = require('fs').promises;
 const path = require('path');
+const { v4  } = require('uuid') ;
 
 const contactsPath = path.resolve('./db/contacts.json');
 
@@ -11,38 +12,48 @@ async function listContacts() {
 };
 
 async function getContactById(id) {
+    const contactId = isNaN(id) ? id : Number(id);
+
     const allContacts = await fs.readFile(contactsPath, 'utf8');
     const parsedContacts = JSON.parse(allContacts);
-    const contactById = parsedContacts.find((contact) => contact.id === Number(id));
+    const contactById = parsedContacts.find((contact) => contact.id === contactId);
+    if (!contactById) {
+        console.log('There is no contact with such an id!');
+        return;
+    }
     console.log(contactById);
     return contactById;
 };
 
-async function removeContact(contactId) {
+async function removeContact(id) {
+    const contactId = isNaN(id) ? id : Number(id);
     const allContacts = await fs.readFile(contactsPath, 'utf8');
     const parsedContacts = JSON.parse(allContacts);
-    const deleteContactById = JSON.stringify(parsedContacts.filter(({ id }) => id !== Number(contactId)));  
-    fs.writeFile(contactsPath, deleteContactById, 'utf8');
+    const deleteContactById = JSON.stringify(parsedContacts.filter((contact) => contact.id !== contactId));
+    await fs.writeFile(contactsPath, deleteContactById, 'utf8');
     console.log(`Contact with id ${contactId} deleted`)
 };
 
 
 async function addContact(name, email, phone) {
-    const prevUsers = await fs.readFile(contactsPath, 'utf8');
-    const parsedPrevUsers = JSON.parse(prevUsers);
+    if (!name || !email || !phone) {
+        console.log('Input: name, email, phone!');
+        return;
+    }
+   const allContacts = await fs.readFile(contactsPath, 'utf8');
+    const parsedPrevUsers = JSON.parse(allContacts);
+    
     const user = {
-        id: parsedPrevUsers.length + 1,
+        id: v4(),
         name,
         email,
         phone,
     };
     
-    if (user.name && user.email && user.phone) {
         const allUsers = parsedPrevUsers.concat([user]);
         const allUsersJSON = JSON.stringify(allUsers);
         fs.writeFile(contactsPath, allUsersJSON, 'utf8');
         console.log(`Contact with name ${name} is added`)
-    }
 };
 
 const contacts = {
